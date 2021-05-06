c44b63c6243976b574e9d9bb0849832d7a2ec21f
Nate Lapinski <n4t311@gmail.com>
2021-05-06 13:29:51 +0900

    unit test setup for config and users service

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,6 +2,12 @@ from flask.cli import FlaskGroup
 
 from project import app, db
 
+import unittest
+# Hack to fix error when trying to run tests
+# https://github.com/jarus/flask-testing/issues/143#issuecomment-602715871
+import werkzeug
+werkzeug.cached_property = werkzeug.utils.cached_property
+
 cli = FlaskGroup(app)
 
 @cli.command()
@@ -9,6 +15,15 @@ def recreate_db():
   db.drop_all()
   db.create_all()
   db.session.commit()
+
+@cli.command()
+def test():
+  """Runs tests w/o code coverage"""
+  tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+  result = unittest.TextTestRunner(verbosity=2).run(tests)
+  if result.wasSuccessful():
+    return 0
+  return 1
 
 if __name__ == '__main__':
   cli()
